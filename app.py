d49ce4f7d8da651e3b007c1ab29fa9a68b3a76ee
Tom Hogans <tomhsx@gmail.com>
2015-02-16 15:33:18 -0500

    Read data from data.txt in app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import queue
+
 from flask import Flask
 app = Flask(__name__)
+
+my_queue = queue.Queue()
+
+with open("data.txt") as f:
+    for s in f.read().splitlines():
+        my_queue.put(s)
 
 @app.route('/')
 def hello_world():
