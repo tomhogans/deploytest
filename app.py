d785d479962a94a6dec9f7829a8cf24b1fe0223a
Tom Hogans <tomhsx@gmail.com>
2015-02-16 15:35:30 -0500

    Cycle through queue when returning response

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-import queue
+import Queue
 
 from flask import Flask
 app = Flask(__name__)
 
-my_queue = queue.Queue()
+my_queue = Queue.Queue()
 
 with open("data.txt") as f:
     for s in f.read().splitlines():
         my_queue.put(s)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def next_item_from_queue():
+    item = my_queue.get()
+    my_queue.put(item)
+    return item
 
 if __name__ == '__main__':
     app.run()
